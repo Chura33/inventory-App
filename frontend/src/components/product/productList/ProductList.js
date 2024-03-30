702658b32e441f9e2513fdb0d6192e6ef4733bbd
Chura33 <churabrady@gmail.com>
2024-03-30 15:49:29 +0100

    created search function

--- frontend/src/components/product/productList/ProductList.js
+++ frontend/src/components/product/productList/ProductList.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductList.scss"
 import SpinnerImg from "../../../components/loader/Loader"
 import {FaEdit, FaTrashAlt} from "react-icons/fa";
 import {AiOutlineEye} from "react-icons/ai";
 import Search from "../../search/Search";
-
+import {useDispatch, useSelector} from "react-redux";
+import { FILTER_PRODUCTS, selectFilteredPoducts } from "../../../redux/features/product/filterSlice";
+import ReactPaginate  from "react-paginate";
 const ProductList = ({products, isLoading}) => {
     const [search, setSearch] = useState("");
+    const filteredProducts = useSelector(selectFilteredPoducts);
+    const dispatch = useDispatch();
     const shortenText = (text, n) => {
         if (text.length > n){
             const shortenedText = text.substring(0, n).concat("...");
@@ -14,6 +18,29 @@ const ProductList = ({products, isLoading}) => {
         }
         return text;
     }
+
+    // BEGIN PAGINATION
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+
+    setCurrentItems(filteredProducts.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(filteredProducts.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, filteredProducts]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % filteredProducts.length;
+    setItemOffset(newOffset);
+  };
+
+    // END PAGINATION
+    useEffect(() =>{
+        dispatch(FILTER_PRODUCTS({products, search}))
+    }, [products, search, dispatch])
     return <div className="product-list">
         <hr />
         <div className="table">
@@ -45,7 +72,7 @@ const ProductList = ({products, isLoading}) => {
                         </thead>
                         <tbody>
                             {
-                                products.map((product, index)=>{
+                                currentItems.map((product, index)=>{
                                     const {_id, name, category, price, quantity,} = product
                                     return (
                                         <tr key={_id}>
@@ -69,6 +96,20 @@ const ProductList = ({products, isLoading}) => {
                     </table>
                 )}
             </div>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="Next"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount = {pageCount}
+                previousLabel = "Prev"
+                renderOnZeroPageCount={null}
+                containerClassName="pagination"
+                  pageLinkClassName="page-num"
+                  previousLinkClassName="page-num"
+                  nextLinkClassName="page-num"
+                  activeLinkClassName="activePage"
+            />
         </div>
     </div>
 }
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from '../redux/features/auth/authSlice';
 import productReducer from '../redux/features/product/productSlice';
-
+import filterReducer from "./features/product/filterSlice";
 
 
 export const store = configureStore({
     reducer:{
         auth:authReducer,
-        product:productReducer
+        product:productReducer,
+        filter: filterReducer
     }
 })
