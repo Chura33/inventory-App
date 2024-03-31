1f97347c227dbd53a776602483dcdd2d2974e67d
Chura33 <churabrady@gmail.com>
2024-03-31 06:29:44 +0100

    untrack node_modules

--- frontend/src/components/product/productSummary/ProductSummary.js
+++ frontend/src/components/product/productSummary/ProductSummary.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./ProductSummary.scss";
+import {AiFillDollarCircle} from "react-icons/ai";
+import {BsCart4, BsCartX} from "react-icons/bs";
+import {BiCategory} from "react-icons/bi";
+import InfoBox from "../../infoBox/InfoBox";
 
-const ProductSummary = () => {
-    return <div>ProductSummary</div>
+// Icons
+const earningIcon = < AiFillDollarCircle size={40} color="#fff"/> 
+const productIcon = <BsCart4 size={40} color="#fff"/>
+const categoryIcon = <BiCategory size={40} color="#fff" />
+const outOfStockIcon = <BsCartX size={40} color="#fff" />;
+const ProductSummary = ({products}) => {
+    return <div className="product-summary">
+        <h3 className="--mt"> Inventory Stats</h3>
+        <div className="info-summary">
+            <InfoBox icon={productIcon} title={"Total Products"} count={products.length} bgColor="card1"/>
+        </div>
+    </div>
 }
 
 export default ProductSummary;
--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import { selectIsLoggedIn } from '../../redux/features/auth/authSlice';
 import { getProducts } from '../../redux/features/product/productSlice';
 import {useEffect} from "react"; 
 import ProductList from '../../components/product/productList/ProductList';
+import ProductSummary from '../../components/product/productSummary/ProductSummary';
 
 const Dashboard = ({children}) => {
   const path = '/login'; // replace with your actual path
@@ -25,6 +26,7 @@ const Dashboard = ({children}) => {
   return (
     <div>
       <h2>Dashboard</h2>
+      <ProductSummary products={products} />
       <ProductList products={products} isLoading={isLoading}/>
       {children}
     </div>
