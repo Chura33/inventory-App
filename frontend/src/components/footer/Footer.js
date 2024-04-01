babf453548e8353946943b222e838525dd2ddec4
Chura33 <churabrady@gmail.com>
2024-04-01 17:18:34 +0100

    added product summary with complete code

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -3,7 +3,7 @@
     const Footer = () => {
       return (
         <div className='--flex-center --py2'>
-            <p>All Rights reserved. &copy; 2023</p>
+            <p>All Rights reserved. &copy; {new Date().getFullYear()}</p>
         </div>
       )
     }
